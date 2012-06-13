ff128b00fda9e83b25ffd8193d9281de84c478f2
Shawn Sandy <shawnsandy04@gmail.com>
2012-06-13 09:29:28 -0400

    beta version commit

--- AdminbarPostMenus.php
+++ AdminbarPostMenus.php
@@ -21,7 +21,7 @@ class AdminbarPostMenus {
             $display_pending = true,
             $display_draft = true,
             $display_schedule = true,
-            $post_types = array('post' => 'Post', 'pages' => 'Pages'),
+            $post_types = array('post' => 'Posts', 'page' => 'Pages'),
             $list_count = 5;
 
     public function set_display_schedule($display_schedule) {
@@ -64,22 +64,26 @@ class AdminbarPostMenus {
             if ($this->display_published):
                 /** Display published pages * */
                 Ext_Post_Menus::add()->set_node_id($post_type . '_menu')->set_node_title(ucfirst($title))
-                        ->set_items($this->list_count)->published($this->post_types);
+                        ->set_items($this->list_count)->set_post_type($post_type)->published($post_type, $this->list_count);
             endif;
 
-            if ($this->display_pending):
-                Ext_Post_Menus::add()->set_node_id($post_type . '_pending')->set_node_parent($post_type . '_menu')
-                        ->set_node_title(ucfirst($title))->published($this->post_types);
-            endif;
 
             if ($this->display_schedule):
                 Ext_Post_Menus::add()->set_node_id($post_type . '_schedule')->set_node_parent($post_type . '_menu')
-                        ->set_items($this->list_count)->set_node_title(ucfirst($title))->published($this->post_types);
+                        ->set_items($this->list_count)->set_node_title('SCHEDULE')->set_post_type($post_type)
+                    ->scheduled($post_type, $this->list_count);
+            endif;
+
+
+            if ($this->display_pending):
+                Ext_Post_Menus::add()->set_node_id($post_type . '_pending')->set_node_parent($post_type . '_menu')
+                        ->set_node_title('PENDING')->set_post_type($post_type)->pending($post_type, $this->list_count);
             endif;
 
             if ($this->display_draft):
                 Ext_Post_Menus::add()->set_node_id($post_type . '_draft')->set_node_parent($post_type . '_menu')
-                        ->set_items($this->list_count)->set_node_title(ucfirst($title))->published($this->post_types);
+                        ->set_items($this->list_count)->set_node_title('DRAFTS')
+                    ->set_post_type($post_type)->drafts($post_type,$this->list_count);
             endif;
         }
     }
